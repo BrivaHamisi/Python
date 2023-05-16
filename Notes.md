cfd5eb6cde2183e4b1a9fa38376a3d5476835bfd
BrivaHamisi <brivahamisi@gmail.com>
2023-05-16 09:48:54 +0300

    Working with Numeric Data

--- Notes.md
+++ Notes.md
@@ -1,1 +1,3 @@
+Slicing - used to get a slice of elements from a collection of elements
+
 A method id a function that belongs to an object
---
+++ integers&floats.py
@@ -0,0 +1,48 @@
+# Integer
+num= 3
+
+# Float
+num =3.14
+print(type(num))
+
+#Arithmetic Operators
+print(3 + 2)   #Addition
+print(3 - 2)  #Subtraction
+print(3/2)   #Division
+print(3*2)  #Multiplication
+print(3//2)  #Floor Division
+print(3**2)  #Exponent
+print(3%2)   #Modulus
+
+
+#Order of operations
+print(3*2+1)
+print(3 * (2+1))
+
+num = 1
+num +=1 
+print(num)
+
+num *= 10
+print(num)
+
+print(abs(-3))
+print(round(3.75, 0))
+
+
+#Comparison Operators
+print(3 == 2)  #Equal to
+print(3 != 2)  #Not Equal to
+print(3 <= 2)  #Less or Equal to
+print(3 >= 2)  #Greater or  Equal to
+print(3 > 2)   #Greater than
+print(3 < 2)   #Less than
+
+num_1 = '100'
+num_2 = '200'
+
+
+num_1 = int(num_1)
+num_2 = int(num_2)
+
+print(num_1 + num_2)
