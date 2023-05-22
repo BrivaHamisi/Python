fc144cf252e222ecfe81d4164a1a79a0cec16bd7
BrivaHamisi <brivahamisi@gmail.com>
2023-05-22 22:05:01 +0300

    Lists Tuples ans Sets

---
+++ Lists Tuples & Sets.py
@@ -0,0 +1,9 @@
+#Lists and tuples allows working with sequential data
+#Sets- Unordered collection of values with no duplicates
+
+#lists
+courses =['History', 'Maths', 'Physics', 'CompSci']
+print(courses)
+print(len(courses))
+print(courses[-1])
+print(courses[0])
