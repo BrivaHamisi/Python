60bb0508451b2287d65dc95a7011f433e1ecd986
BrivaHamisi <brivahamisi@gmail.com>
2023-06-19 23:20:26 +0300

    Import Modules

--- Import Modules/Intro.py
+++ Import Modules/Intro.py
@@ -1,7 +1,10 @@
 
 import My_Modules
+from My_Modules import find_index, test
 
 courses = ['History', 'Math', 'Physics', 'CompSci']
 
 index = My_Modules.find_index(courses, 'Math')
-print(index)
+print(index )
+
+print(test)
